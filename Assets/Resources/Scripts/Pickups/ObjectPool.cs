b01e1bd96af6cc1005848e63082178bbd8ba8992
Daikone <petorikoru@outlook.com>
2021-03-01 20:15:11 +0100

    created one room and pickup spawner

---
+++ Assets/Resources/Scripts/Pickups/ObjectPool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] GameObject Pickup;
+    [SerializeField] float spawnTimer = 1f;
+    [SerializeField] float spawnRadius = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnPickup());
+    }
+
+    IEnumerator SpawnPickup()
+    {
+        while(true)
+        {
+            Vector3 position = new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius)) + transform.position;
+            Instantiate(Pickup, position, Quaternion.identity, transform);
+
+            // Instantiate(Pickup, transform);
+            yield return new WaitForSeconds(spawnTimer);
+        }
+    }
+}
