04c46faeec31650538eda0ff43f8cb91004aae99
Mattes Fischer <mattes.fischer@gmx.net>
2021-03-10 15:08:20 +0100

    Adding New Behaviour into Ghost Behaviour

--- Assets/Resources/Scripts/Pickups/GhostBehaviour.cs
+++ Assets/Resources/Scripts/Pickups/GhostBehaviour.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GhostBehaviour : MonoBehaviour
+[RequireComponent(typeof(CapsuleCollider))]
+public class GhostBehaviour : BaseGhostAI
 {
     public bool hasPickup = false;
     public int speed = 1;
 
+    
+    //   !!!!!!! Matti Stuff !!!!!!!!!
+    public string YourName;
+    private SphereCollider _trigger;
+    private CapsuleCollider _collider;
+    private GameObject Cauldron;
+    private LayerMask humanLayerCheck;
+    private  List<GameObject> humansInSight = new List<GameObject>();
+    private GameObject ClosestHuman;
+    
+    
+    private void Awake()
+    {
+        // Matti Stuff
+        Cauldron = GameObject.Find("Cauldron");
+        humanLayerCheck = LayerMask.GetMask("Humans");
+        carryingSouls = 0;
+        isStunned = false;
+        
+    }
+    
+    
     // Update is called once per frame
     void Update()
     {
@@ -15,5 +37,33 @@ public class GhostBehaviour : MonoBehaviour
             hasPickup = false;
             GetComponent<PickupBehaviour>().usePickup();
         }
+        
+        
+        
+        
+        // Matti Stuff
+        humansInSight = CheckCloseObjectsInSight(gameObject, 5f, humanLayerCheck);
+        if (humansInSight.Count > 0)
+        {
+            ClosestHuman = ClosestObjectInList(gameObject, humansInSight);
+        }
+
+        if (Vector3.Distance(gameObject.transform.position, Cauldron.transform.position) <= 5 && carryingSouls > 0)
+        {
+            Debug.Log("Kessel");
+            carryingSouls = 0;
+        }
+        if (ClosestHuman != null && Vector3.Distance( gameObject.transform.position, ClosestHuman.transform.position) <= 1)
+        {
+            Destroy(ClosestHuman);
+            carryingSouls++;
+        }
     }
+    
+    
+    
+    
+    
+
+    
 }
