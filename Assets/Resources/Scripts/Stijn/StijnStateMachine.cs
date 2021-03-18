f467603e3193dfa4e77654b745a74814b924e42f
Stijn Wiltvank <stijnemw@gmail.com>
2021-03-18 08:46:51 +0100

    Ghost AI basic scripts

---
+++ Assets/Resources/Scripts/Stijn/StijnStateMachine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Resources.Scripts.Stijn
+{
+    public class StijnStateMachine
+    {
+        public StijnState CurrentState { get; private set; }
+
+        public void Initialize(StijnState startingState)
+        {
+            CurrentState = startingState;
+            startingState.Enter();
+        }
+
+        public void ChangeState(StijnState newState)
+        {
+            CurrentState.Exit();
+
+            CurrentState = newState;
+            newState.Enter();
+        }
+    }
+}
+
